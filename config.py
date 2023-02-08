380c531e62000d3e4741f7c1464df7bbe02e1b95
Amirreza-shj <116149087+Amirreza-shj@users.noreply.github.com>
2023-02-08 19:44:57 +0100

    change package structure

---
+++ config.py
@@ -0,0 +1,6 @@
+"""add all the configurations like global variable"""
+
+CSV_PATH = {
+    'reservoir': './data/volume_and_stage_hydrograph_of_pool_Beta.csv',
+    'water_level': './data/water_level_info.csv'
+}
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from PumpStorage import PumpStorage
-from WaterLevel import WaterLevel
-ps_obj=PumpStorage.PumpStorage(csv_file="PumpStorage/volume and stage hydrograph of pool Beta.csv")
-wl_obj=WaterLevel.WaterLevel(csv_file="WaterLevel/water_level_info.csv")
-print(f"volume of pool Beta is: {ps_obj.maxVolume()-ps_obj.minVolume()}")
-ps_obj.plotVolume()
-ps_obj.findPeaks()
-print(f"pump storage volume in lake is {ps_obj.maxVolume()-ps_obj.minVolume()}")
-print(f"maximum operational water level in reservoir is {wl_obj.maxWaterAlphaLake()}")
-print(f"bottom outlet is located at {wl_obj.bottomOutlet()}")
-print(f"annual storage volume of reservoir is volume of reservoir without dead volume and pump storage so in this case ---> {wl_obj.annualStorageVolume(maxheight=wl_obj.maxWaterAlphaLake(),minheight=wl_obj.minWaterAlphaLake(),pumpvolume=ps_obj.maxVolume()-ps_obj.minVolume())}")
+from reservoir.pump_storage import PumpStorage
+from reservoir.water_level import WaterLevel
+from config import CSV_PATH
+
+
+if __name__ == '__main__':
+    ps_obj = PumpStorage(csv_file=CSV_PATH['reservoir'])
+    wl_obj = WaterLevel(csv_file=CSV_PATH['water_level'])
+    print(f"volume of pool Beta is: {ps_obj.maxVolume() - ps_obj.min_volume()}")
+    ps_obj.plotVolume()
+    ps_obj.findPeaks()
+    print(f"pump storage volume in lake is {ps_obj.maxVolume() - ps_obj.min_volume()}")
+    print(f"maximum operational water level in reservoir is {wl_obj.maxWaterAlphaLake()}")
+    print(f"bottom outlet is located at {wl_obj.bottomOutlet()}")
+    print(
+        f"annual storage volume of reservoir is volume of reservoir without dead volume and pump storage so in this case ---> {wl_obj.annualStorageVolume(maxheight=wl_obj.maxWaterAlphaLake(), minheight=wl_obj.minWaterAlphaLake(), pumpvolume=ps_obj.maxVolume() - ps_obj.min_volume())}")
